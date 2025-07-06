267325eb28b7eb8233f02770e5753a34b8d2ccfd
Vinay372005 <ee24b006@iittp.ac.in>
2025-07-06 08:14:29 +0530

    Create scoreRoutes.js

---
+++ routes/scoreRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const Score = require("../models/Score");
+
+router.get("/", async (req, res) => {
+  const score = await Score.findOne().sort({ _id: -1 });
+  res.json(score);
+});
+
+router.post("/", async (req, res) => {
+  const { password, runs, wickets, overs, status } = req.body;
+  if (password !== process.env.ADMIN_PASS) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const newScore = new Score({ runs, wickets, overs, status });
+  await newScore.save();
+  res.status(200).json(newScore);
+});
+
+module.exports = router;
