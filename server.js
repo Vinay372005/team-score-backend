1612d2f28817e4b0cecf3c3a693e00192951f182
Vinay372005 <ee24b006@iittp.ac.in>
2025-07-06 08:07:46 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const playerRoutes = require("./routes/playerRoutes");
+const scoreRoutes = require("./routes/scoreRoutes");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/players", playerRoutes);
+app.use("/api/scores", scoreRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB error", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
