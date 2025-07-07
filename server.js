412004c3cc0d02cf6a4ae9d114065ad66dcc64de
Vinay372005 <ee24b006@iittp.ac.in>
2025-07-07 08:58:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use('/uploads', express.static('uploads'));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
