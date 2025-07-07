572ec995d3b3b58180ebc83e7e445e3b251f98b7
Vinay372005 <ee24b006@iittp.ac.in>
2025-07-07 09:50:27 +0530

    Update player.js

--- models/player.js
+++ models/player.js
@@ -1,11 +1,22 @@
-
 import mongoose from 'mongoose';
 
 const playerSchema = new mongoose.Schema({
-  name: String,
-  role: String,
-  phone: String,
-  image: String 
+  name: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
 });
 
 const Player = mongoose.model('Player', playerSchema);
